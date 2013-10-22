3cc64133e48929daaff560de1f73f46d1853e931
dadachi <maurois@mac.com>
2013-10-22 18:12:29 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareup::Application.config.secret_key_base = '703b0c54af4f15cd567878ebbc486ebc2ec0ae41e8f555d49f57998dd12f4f13921c5d853b672d39ac3da71f526d2a48236322fcec391ebb09a600223df32b00'
